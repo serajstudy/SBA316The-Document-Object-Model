c4ad0dc73af634c019ca02dbfbd233b1ad4af284
seraj <serajstudy1@gmail.com>
2025-08-17 12:18:32 -0400

    commit

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// Cache elements
+
+let titleEl = document.getElementById(`title`);
+let form = document.querySelector(`#todoform`);
+let input =  document.querySelector (`#todoinput`);
+let todoList = document.querySelector(`#todolist`);
+let reloadBtn =document.getElementById (`reloadBtn`);
+
+
+// console.log(`window width: `+ window.innerWidth);
+// alert(`Welcome to the To Do App!`);
+
+// add form event listener
+
+form.addEventListener(`submit`,function(e){
+    e.preventDefault();
+    
+    if (input.ariaValueMax.length<3){
+        alert ("Task must be 3 character long!");
+        return;
+    }
+
+    // create new element
+
+    let li = document.createElement(`li`);
+
+    li.textContent = input.value;
+
+    //  add to dom
+    todoList.appendChild(li);
+
+    li.setAttribute(` title,` `click to mark as done`);
+
+    input.value = "";
+
+
+});
+
+
+
+
+
